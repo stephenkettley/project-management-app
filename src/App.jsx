8685eabbe588fc1bbfff1cc69c28b0fe985c7e2d
Stephen Kettley <kettleystephen@gmail.com>
2023-11-26 12:55:03 +0200

    update main app page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null}
+
+    })
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+     content = <NewProject></NewProject>
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}></NoProjectSelected>
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleStartAddProject}></ProjectsSidebar>
+      {content}
+    </main>
   );
 }
 
