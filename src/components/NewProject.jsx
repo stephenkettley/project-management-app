7c2bab49a4e246bb93608c14fb8e828c98a627ff
Stephen Kettley <kettleystephen@gmail.com>
2023-11-27 15:14:35 +0200

    handle storing user input for new project

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,15 +1,32 @@
+import { useRef } from "react";
 import InputBox from "./InputBox.jsx";
 
-const NewProject = () => {
+const NewProject = ({ onAdd }) => {
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+
+    function handleSave() {
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate = dueDate.current.value;
+
+        onAdd({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: enteredDueDate
+        })
+    }
+
     return <div className="w-[35rem] mt-16">
         <div>
-           <InputBox label="Title"></InputBox>
-           <InputBox label="Description" textArea></InputBox>
-           <InputBox label="Due Date"></InputBox>
+           <InputBox type="text" label="Title" ref={title}></InputBox>
+           <InputBox label="Description" textArea ref={description}></InputBox>
+           <InputBox type="date" label="Due Date" ref={dueDate}></InputBox>
         </div>
         <menu className="flex items-center justify-end gap-4 my-4">
             <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-            <li><button className=" px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
+            <li><button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
         </menu>
     </div>
 }
