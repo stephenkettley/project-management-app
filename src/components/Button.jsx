89811dcef2db2e32436ca00142327e8420307906
Stephen Kettley <kettleystephen@gmail.com>
2023-11-28 14:01:14 +0200

    adjust button styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 const Button = ({ children, ...props }) => {
-    return <button {...props} className="px-8 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+    return <button {...props} className="px-8 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-200 hover:bg-stone-600 hover:text-stone-100">
             {children}
     </button>
 }
