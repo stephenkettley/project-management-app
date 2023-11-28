7a5436c2a5a06f4f87e185219586d00a953241d9
Stephen Kettley <kettleystephen@gmail.com>
2023-11-28 14:01:26 +0200

    validate input for new project

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,9 @@
 import { useRef } from "react";
 import InputBox from "./InputBox.jsx";
+import Modal from "./Modal.jsx";
 
-const NewProject = ({ onAdd }) => {
+const NewProject = ({ onAdd, onCancel }) => {
+    const modal = useRef();
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -11,6 +13,15 @@ const NewProject = ({ onAdd }) => {
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
 
+        if (
+            enteredTitle.trim() === "" || 
+            enteredDescription.trim() === "" || 
+            enteredDueDate.trim() === ""
+        ) {
+            modal.current.open();
+            return;
+        }
+
         onAdd({
             title: enteredTitle,
             description: enteredDescription,
@@ -18,17 +29,24 @@ const NewProject = ({ onAdd }) => {
         })
     }
 
-    return <div className="w-[35rem] mt-16">
+    return <>
+        <Modal ref={modal} buttonCaption="Close">
+            <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+            <p className="text-stone-900 mb-2">Oops! Looks like you forgot to enter a value!</p>
+            <p className="text-stone-900 mb-2">Please make sure you provide a valid value for every input.</p>
+        </Modal>
+        <div className="w-[35rem] mt-16">
         <div>
            <InputBox type="text" label="Title" ref={title}></InputBox>
            <InputBox label="Description" textArea ref={description}></InputBox>
            <InputBox type="date" label="Due Date" ref={dueDate}></InputBox>
         </div>
         <menu className="flex items-center justify-end gap-4 my-4">
-            <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+            <li><button onClick={onCancel} className="text-stone-800 hover:text-stone-950">Cancel</button></li>
             <li><button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
         </menu>
-    </div>
+        </div>
+    </>
 }
 
 export default NewProject;
